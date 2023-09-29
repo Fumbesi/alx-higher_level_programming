f2aeb6f7c05642926b4a0cdd8a85aad2a53eb6cf
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-09-29 04:00:20 -0700

    5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Send a POST request with parameters and display the body of the response
-# Set the URL and POST data
-url="$1"
-email="test@gmail.com"
-subject="I will always be here for PLD"
 curl -s -X POST -d "email=$email&subject=$subject" "$url"
