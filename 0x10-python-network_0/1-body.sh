2ed4ad9b76763b1fa5e776dc6b54213d2529e500
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-09-29 03:04:53 -0700

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # This script sends a GET request to a URL, retrieves and displays the body of the response if it has a 200 status code.
-
 # Send a GET request to the URL, discard headers, and display the body for a 200 status code response
 curl -s -X GET -I "$1" | awk 'NR==1{if($2 == 200) system("curl -s "$1)}'
