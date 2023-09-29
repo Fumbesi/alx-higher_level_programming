33adfd5eece70bf29f28d306ffa62004cd97a723
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-09-29 03:14:37 -0700

    01

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # Send a GET request to the URL, discard headers, and display the body for a 200 status code response
 curl -s -X GET -I "$1" | awk 'NR == 1 {if ($2 == 200) {body=1} else {body=0}} NR > 1 {if (body) print}'
