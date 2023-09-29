63339390acf30eb5976d9c5f32039ba7ba75acfe
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-09-29 04:18:35 -0700

    100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
 # Send a GET request to the URL and display only the status code
-curl -s -o /tmp/status_code.txt -w "%{http_code}" "$1"
-
-# Display the status code
-cat /tmp/status_code.txt
-
-# Clean up the temporary file
-rm -f /tmp/status_code.txt
+curl -s -o /tmp/status_code.txt -w "%{http_code}" "$"
