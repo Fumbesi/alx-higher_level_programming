d4698af4bc3ae86d34f9735140bda512bd838036
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-10-24 08:36:28 -0700

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,16 +2,18 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterId = 18; // Wedge Antilles' character ID
+const characterId = 18; // Wedge Antilles character ID
 
 request(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    const filmsData = JSON.parse(body).results;
-    const moviesWithWedgeAntilles = filmsData.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-
-    console.log(moviesWithWedgeAntilles.length);
+    const filmsData = JSON.parse(body);
+    const count = filmsData.results.reduce((acc, film) => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+    console.log(count);
   } else {
     console.error('Error:', error);
   }
