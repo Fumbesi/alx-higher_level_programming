f5b752a3d1ed94996755296a0034f9894186485c
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2023-10-25 08:34:40 -0700

    adding

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-const apiUrl = process.argv[2];
-const characterId = 18; // Wedge Antilles character ID
-
-request(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const filmsData = JSON.parse(body);
-    const count = filmsData.results.reduce((acc, film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        return acc + 1;
-      }
-      return acc;
-    }, 0);
-    console.log(count.toString()); // Print the count as a string
-  } else {
-    console.error('Error:', error);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (!err) {
-        console.log(`Content saved to ${filePath}`);
-      } else {
-        console.error(err);
-      }
-    });
-  } else {
-    console.error('Error:', error);
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
